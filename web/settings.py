f38efac5631e797c5dc27821166204050fe9385d
Giza Jirndhara <87406316+GizaJ0811@users.noreply.github.com>
2023-04-03 10:14:09 +0700

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chexo_app.apps.Chexo_appConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
